fd7ced2667e44b016bace7db5236649f8322bf3c
Thiago Okuyama <thiokue@gmail.com>
2023-04-17 17:59:33 -0300

    file_downloader updated
    Added a feature that doesnt allow a new file to be downloaded if file already exists

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pytube import Search
 import os
 from loguru import logger
 from time import sleep
+import sys
 
 logger.add('logfile.log', rotation='500 MB', compression='zip')
 
@@ -11,14 +12,18 @@ def file_downloader(ch_songs_dir):
     for music_name in dir_list:              #Loops through each folder in your songs directory.
         s_query = Search(music_name).results #Searchs yt for an corresponding video title.
         video = s_query[0]                   #Takes the 1st video 
-        try:
-            logger.info(f'Downloading mv for {music_name}')
-            output_path = os.path.join(ch_songs_dir, music_name)            
-            #download the video at the highest available quality on the specified path, with the correct naming for CH
-            video.streams.get_highest_resolution().download(output_path=output_path, filename='video.mp4')
-            logger.success('Video downloaded!')
-            sleep(1)
-        except Exception as e:
-            logger.error(e)
+        output_path = os.path.join(ch_songs_dir, music_name)
+        chk_path = os.path.join(output_path, 'video.mp4')            
+        if os.path.exists(chk_path) == False:
+            try:
+                logger.info(f'Downloading mv for {music_name}')
+                #download the video at the highest available quality on the specified path, with the correct naming for CH
+                video.streams.get_highest_resolution().download(output_path=output_path, filename='video.mp4')
+                logger.success('Video downloaded!')
+                sleep(1)
+            except Exception as e:
+                logger.error(e)
+        else:
+            logger.error(f'File already exists! at {chk_path}')
 
 file_downloader(r'C:\Users\Thiago\Documents\Clone Hero\Songs')
